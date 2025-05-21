59005ea3cc3a930c71ade06532dcd232ac07e93c
wandera-chris <98383480+wandera-chris@users.noreply.github.com>
2025-05-21 08:36:05 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,12 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+my_list.extend([50, 60, 70])
+my_list.pop()
+my_list.sort()
+index_30 = my_list.index(30)
+print(f"The index of 30 is: {index_30}")
+print(f"Final list: {my_list}")
